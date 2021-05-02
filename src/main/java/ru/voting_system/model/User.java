5359ec38753ebfbf7fdf5853891aaec0d15589f9
Alexiuss <kuzma.prutckoff@yandex.ru>
2021-05-02 19:51:01 +0300

    Changed read/write access for User fields & added tests.

--- src/main/java/ru/voting_system/model/User.java
+++ src/main/java/ru/voting_system/model/User.java
@@ -1,6 +1,7 @@
 package ru.voting_system.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
@@ -27,10 +28,13 @@ public class User extends AbstractNamedEntity {
     @Column(name = "password", nullable = false)
     @NotBlank
     @Size(min = 5, max = 100)
+    // https://stackoverflow.com/a/12505165/548473
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()")
     @NonNull
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date registered = new Date();
 
     @Column(name = "enabled", nullable = false, columnDefinition = "bool default true")
--- src/test/java/ru/voting_system/web/json/JsonUtilTest.java
+++ src/test/java/ru/voting_system/web/json/JsonUtilTest.java
@@ -1,11 +1,17 @@
 package ru.voting_system.web.json;
 
 import org.junit.jupiter.api.Test;
+import ru.voting_system.TestData.UserTestData;
+import ru.voting_system.model.User;
 import ru.voting_system.model.Vote;
 
 import java.util.List;
 
 import static ru.voting_system.TestData.VoteTestData.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JsonUtilTest {
 
@@ -23,5 +29,16 @@ class JsonUtilTest {
         System.out.println(json);
         List<Vote> votes = JsonUtil.readValues(json, Vote.class);
         VOTE_MATCHERS.assertMatch(votes, VOTES);
+    }
+
+    @Test
+    void testWriteOnlyAccess() throws Exception {
+        String json = JsonUtil.writeValue(UserTestData.USER);
+        System.out.println(json);
+        assertThat(json, not(containsString("password")));
+        String jsonWithPass = JsonUtil.writeAdditionProps(UserTestData.USER, "password", "newPass");
+        System.out.println(jsonWithPass);
+        User user = JsonUtil.readValue(jsonWithPass, User.class);
+        assertEquals(user.getPassword(), "newPass");
     }
 }
